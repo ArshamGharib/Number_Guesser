462c09a5923e1e018e3ae1b70a1ee362e4bbeaa2
Arsham Gharib <Arsham.gharib4611@gmail.com>
2024-11-03 01:26:15 +0330

    step 2

--- way1/main.py
+++ way1/main.py
@@ -1,3 +1,18 @@
 import random
 rand_num = random.randint(1, 100)
 
+while True:
+    user_guess = input('Guess a number between 1 and 100: ')
+    if user_guess == 'q':
+        print('Thank you for playing. Goodbye!')
+        break
+    elif not user_guess.isdigit():
+        print('Invalid input!! Please try again.')
+        continue
+
+    user_guess = int(user_guess)
+    if user_guess > 100 or user_guess < 1 :
+        print('Number is out of range, Choose number between 1 to 100!')
+        continue
+
+    print(user_guess)
